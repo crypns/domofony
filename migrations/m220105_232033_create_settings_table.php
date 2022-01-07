5e91306b8590dc81c8db52ed63f63943320b0625
crypns <crypns@yandex.ru>
2022-01-07 15:47:33 +0300

    Migration and fixture for table settings

--- migrations/m220105_232033_create_settings_table.php
+++ migrations/m220105_232033_create_settings_table.php
@@ -16,18 +16,32 @@ class m220105_232033_create_settings_table extends Migration
 
         $this->createTable('{{%settings}}', array_merge([
             'id' => $this->primaryKey(),
-            'number_phone_1' => $this->string()->defaultValue('+38 044 344 65 14')->comment('Номер телефона 1'),
-            'number_phone_2' => $this->string()->defaultValue('+38 067 829 60 20')->comment('Номер телефона 2'),
-            'address' => $this->string()->defaultValue('04209 Київ, вул. Героїв Дніпра, 18')->comment('Адрес'),
-            'email' => $this->string()->defaultValue('info@domofony.ua')->comment('Почта'),
-            'bot_token' => $this->string()->defaultValue('5040707194:AAFhxoJaIu3syh6__OmLmghaM3gdzjIeeaw')->comment('Telegram token bot'),
-            'chat_id' => $this->string()->defaultValue('469111794')->comment('Telegram chat id'),
-            'public_key' => $this->string()->defaultValue('sandbox_i92749472824')->comment('Public key liqpay'),
-            'private_key' => $this->string()->defaultValue('sandbox_vly1EuFgkoSewqeATw4pl9fNtWZOJpCIqAIaHD8M')->comment('Private key liqpay'),
-            'youtube_link' => $this->string()->defaultValue('https://www.youtube.com/user/domofonycomua')->comment('Youtube link'),
-            'facebook_link' => $this->string()->defaultValue('https://www.facebook.com/domofony.com.ua')->comment('Facebook link'),
-            'instagram_link' => $this->string()->defaultValue('https://www.instagram.com/domofony.com.ua/')->comment('Instagram link'),
+            'number_phone_1' => $this->string()->defaultValue(null)->comment('Номер телефона 1'),
+            'number_phone_2' => $this->string()->defaultValue(null)->comment('Номер телефона 2'),
+            'address' => $this->string()->defaultValue(null)->comment('Адрес'),
+            'email' => $this->string()->defaultValue(null)->comment('Почта'),
+            'bot_token' => $this->string()->defaultValue(null)->comment('Telegram token bot'),
+            'chat_id' => $this->string()->defaultValue(null)->comment('Telegram chat id'),
+            'public_key' => $this->string()->defaultValue(null)->comment('Public key liqpay'),
+            'private_key' => $this->string()->defaultValue(null)->comment('Private key liqpay'),
+            'youtube_link' => $this->string()->defaultValue(null)->comment('Youtube link'),
+            'facebook_link' => $this->string()->defaultValue(null)->comment('Facebook link'),
+            'instagram_link' => $this->string()->defaultValue(null)->comment('Instagram link'),
         ],$timestampColumns));
+
+        $this->insert('settings', [
+            'number_phone_1' => '+38 044 344 65 14',
+            'number_phone_2' => '+38 067 829 60 20',
+            'address' => '04209 Київ, вул. Героїв Дніпра, 18',
+            'email' => 'info@domofony.ua',
+            'bot_token' => '5040707194:AAFhxoJaIu3syh6__OmLmghaM3gdzjIeeaw',
+            'chat_id' => '469111794',
+            'public_key' => 'sandbox_i92749472824',
+            'private_key' => 'sandbox_vly1EuFgkoSewqeATw4pl9fNtWZOJpCIqAIaHD8M',
+            'youtube_link' => 'https://www.youtube.com/user/domofonycomua',
+            'facebook_link' => 'https://www.facebook.com/domofony.com.ua',
+            'instagram_link' => 'https://www.instagram.com/domofony.com.ua/',
+        ]);
     }
 
     /**
@@ -35,6 +49,7 @@ class m220105_232033_create_settings_table extends Migration
      */
     public function safeDown()
     {
+        $this->delete('settings', ['id' => 1]);
         $this->dropTable('{{%settings}}');
     }
 }
---
+++ tests/unit/fixtures/data/setting.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'setting0' => [
+        'number_phone_1' => '+38 044 344 65 14',
+        'number_phone_2' => '+38 067 829 60 20',
+        'address' => '04209 Київ, вул. Героїв Дніпра, 18',
+        'email' => 'info@domofony.ua',
+        'bot_token' => '5040707194:AAFhxoJaIu3syh6__OmLmghaM3gdzjIeeaw',
+        'chat_id' => '469111794',
+        'public_key' => 'sandbox_i92749472824',
+        'private_key' => 'sandbox_vly1EuFgkoSewqeATw4pl9fNtWZOJpCIqAIaHD8M',
+        'youtube_link' => 'https://www.youtube.com/user/domofonycomua',
+        'facebook_link' => 'https://www.facebook.com/domofony.com.ua',
+        'instagram_link' => 'https://www.instagram.com/domofony.com.ua/',
+    ],
+];
