601b97bc01ecf6e392a8d8405720ff0e7307cd40
ioSeoGio <ioseogio@gmail.com>
2022-02-20 15:06:09 +0300

    Fix routes

--- config/web.php
+++ config/web.php
@@ -68,11 +68,18 @@ $config = array_merge($language, [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+
                 'api/<controller>' => 'admin/api/<controller>',
                 'api/<controller>/<action>' => 'admin/api/<controller>/<action>',
                 
                 'api/<controller>' => 'api/<controller>',
                 'api/<controller>/<action>' => 'api/<controller>/<action>',
+
+                '' => 'site/index',
+                '<action:[\w-]+>' => 'site/<action>'
+                '<controller:[\w-]+>' => '<controller>/index',
+                '<controller:[\w-]+>/<id:\d+>' => '<controller>/view',
+                '<controller:[\w-]+>/<id:\d+>/<action:[\w-]+>' => '<controller>/<action>',
             ],
             'ignoreLanguageUrlPatterns' => [
                 // route pattern => url pattern
